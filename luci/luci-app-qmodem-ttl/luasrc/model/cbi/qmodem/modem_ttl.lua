566e0f55ea32f4fc20ebaa1abcb601bde3b7f797
Doty The Cat <dotywrt@gmail.com>
2026-01-24 02:42:23 +0800

    Update modem_ttl.lua

--- luci/luci-app-qmodem-ttl/luasrc/model/cbi/qmodem/modem_ttl.lua
+++ luci/luci-app-qmodem-ttl/luasrc/model/cbi/qmodem/modem_ttl.lua
@@ -1,13 +1,18 @@
-local uci = luci.model.uci.cursor()
-m = Map("qmodem_ttl", translate("TTL Config"))
+uci = luci.model.uci.cursor()
+
+m = Map("qmodem_ttl", translate("TTL / Hop Limit Config"))
+
 s = m:section(NamedSection, "main", "main", translate("Global Config"))
 
 enable = s:option(Flag, "enable", translate("Enable"))
 enable.default = "0"
 
-ttl = s:option(Value, "ttl", translate("TTL"))
-ttl.default = 64
-ttl.datatype = "uinteger"
+ttl4 = s:option(Value, "ttl4", translate("IPv4 TTL"))
+ttl4.default = 64
+ttl4.datatype = "uinteger"
 
+hl6 = s:option(Value, "hl6", translate("IPv6 Hop Limit"))
+hl6.default = 65
+hl6.datatype = "uinteger"
 
 return m
